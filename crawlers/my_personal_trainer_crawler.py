e248a59e32a96fb0caa7ab6e26e68717179e21ac
Tommaso Fontana <zommy21@hotmail.it>
2018-11-17 14:18:11 +0100

    redownloaded my personal trainer

--- crawlers/my_personal_trainer_crawler.py
+++ crawlers/my_personal_trainer_crawler.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup, SoupStrainer
 import pandas as pd
 import requests
 from requests import Response
+import logging
 from urllib.parse import urlparse
 
 import os
 import json
 import bs4
 import pandas as pd
-
 
 def html_sanitization(html: str) -> str:
     """Return sanitized html."""
@@ -32,7 +32,10 @@ def get_product_calories(soup: BeautifulSoup) -> str:
 
 def parse_tables(html: str, path: str, strainer: SoupStrainer):
     """Parse table at given strained html object saving them as csv at given path."""
-    for i, df in enumerate(pd.read_html(html_sanitization(html), decimal=",", thousands="")):
+    tables = pd.read_html(html_sanitization(html), decimal=",", thousands="")
+    if len(tables) != 3:
+        return
+    for i, df in enumerate(tables):
         df.to_csv("{path}/{table_name}.csv".format(path=path, table_name=i))
 
 
@@ -54,6 +57,7 @@ def parse(response: Response):
         root=urlparse(response.url).netloc, product=get_product_name(response))
     if not os.path.exists(path):
         os.makedirs(path)
+
     parse_tables(
         response.text, path,
         SoupStrainer(
@@ -62,14 +66,19 @@ def parse(response: Response):
 
     parse_metadata(
         response.text, path)
+
+
 def url_validator(url: str, logger: Log, statistics: Statistics)->bool:
     """Return a boolean representing if the crawler should parse given url."""
-    return url.lower().startswith("https://www.my-personaltrainer.it/tabelle")
+    return url.lower().startswith("https://www.my-personaltrainer.it/tabelle") and "#" not in url
 
 
 def file_parser(response: Response, logger: Log, statistics):
     if response.url.endswith(".htm") or response.url.endswith(".html"):
-        parse(response)
+        try:
+            parse(response)
+        except:
+            logger.error(response.url)
 
 
 seed = "https://www.my-personaltrainer.it/tabelle-nutrizionali.htm"
