b8949f2bec90632c79589c1cc1ceaa34efa5f19d
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-11-28 13:32:24 +0100

    Fixed dataframe tfidf

--- merger/metrics/tfidf/dataframes_tfidf_cosine_similarity.py
+++ merger/metrics/tfidf/dataframes_tfidf_cosine_similarity.py
@@ -6,9 +6,13 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 def dataframes_tfidf_cosine_similarity(A: pd.DataFrame, B: pd.DataFrame, vectorizer: TfidfVectorizer):
-    try:
-        return type_argsort(None, cosine_similarity(*[
-            [np.mean(vectorizer.transform(c), axis=0) for c in compact(string(df))] for df in (A, B)
-        ]), A, B, fill=1)
-    except (ValueError, AttributeError):
-        return np.ones((A.shape[1], B.shape[1]))
+    a, b = [
+        np.array([np.mean(vectorizer.transform(c), axis=0) for c in compact(string(df))]) for df in (A, B)
+    ]
+
+    if a.size == 0 or b.size == 0:
+        return np.zeros((A.shape[1], B.shape[1]))
+
+    similarity = cosine_similarity(a.squeeze(axis=1), b.squeeze(axis=1))
+
+    return type_argsort(None, similarity, A, B)
