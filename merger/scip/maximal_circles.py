de4a2445346ef61f6c3cce2289ea3101543ff79e
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-11-29 07:46:43 +0100

    Simplified linear model

--- merger/scip/maximal_circles.py
+++ merger/scip/maximal_circles.py
@@ -1,25 +1,19 @@
 from pyscipopt import Model, quicksum
+import numpy as np
 
 
-def maximal_circles_model(rewards):
+def maximal_circles_model(rewards: np.ndarray):
     model = Model("Maximal Circles")
     N = rewards.shape[1]
     x = {(i, j): model.addVar(vtype="B", name="x({i},{j})".format(i=i, j=j))
-         for i in range(N) for j in range(N)}
+         for i in range(N) for j in range(N) if not np.isclose(rewards[i, j], 0)}
 
-    for i in range(N):
+    for i, _ in x:
         model.addCons(quicksum(x[i, j]
-                               for j in range(N)) <= 1, "Circularity({i})".format(i=i))
-        model.addCons(quicksum(x[i, j]
-                               for j in range(N)) == quicksum(x[j, i]
-                                                              for j in range(N)), "Circularity({i})".format(i=i))
-
-    model.addCons(quicksum(x[i, j] for i in range(N) for j in range(
-        N) if rewards[i, j] == 0) == 0, "NoZeroRewards")
-    model.addCons(quicksum(x[i, i] for i in range(N)) == 0, "Selfloops")
-
+                               for k, j in x if k == i) <= 1, "Unitary({i})".format(i=i))
+        model.addCons(quicksum(x[i, j] - x[j, i]
+                               for k, j in x if k == i) == 0, "Circularity({i})".format(i=i))
     model.setObjective(quicksum(x[i, j]*rewards[i, j]
-                                for i in range(N)
-                                for j in range(N)), "maximize")
+                                for i, j in x), "maximize")
     model.data = x
     return model
